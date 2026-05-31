374084e170dcfee94121b1934e8823078e591d9a
nhantq2005 <nhan.tq2005@gmail.com>
2026-05-31 21:12:31 +0700

    update chat btn

--- src/components/ChatButton.js
+++ src/components/ChatButton.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import { chatButtonStyle } from "../style/ChatButtonStyle";
 
 const ChatButton = () => {
 	const [hover, setHover] = React.useState(false);
-    const nav = useNavigate();
+	const nav = useNavigate();
+	const location = useLocation();
+	const path = location.pathname;
+	const isHidden = path.startsWith("/librarian") || path === "/login" || path === "/register" || path === "/message-client";
+
+	if (isHidden) return null;
+
 	return (
 		<button
 			style={hover ? { ...chatButtonStyle.buttonStyle, ...chatButtonStyle.buttonHoverStyle } : chatButtonStyle.buttonStyle}
